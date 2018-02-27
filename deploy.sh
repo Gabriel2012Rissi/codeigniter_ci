504f7b2b828040b7ead7f86a0cc6bf8542b3c948
Gabriel2012Rissi <gabriel2012rissi@gmail.com>
2018-02-27 18:25:41 -0300

    Primeiro envio

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+DIRECTORY=${PWD}/.docker
+
+dockerArray=("secrets")
+
+if [ -d "$DIRECTORY" ]; then
+  echo "docker directory already exists..."
+  exit 0
+else
+  for dockerPaths in "${dockerArray[@]}"
+  do
+    mkdir -p "$DIRECTORY/$dockerPaths"
+  done
+  if [ -d "$DIRECTORY/secrets" ]; then
+    echo "codeigniter" >> "$DIRECTORY/secrets/db_user.txt"
+    gpg --gen-random --armor 1 14 >> "$DIRECTORY/secrets/db_password.txt"
+  fi
+  echo "docker directory was created successfully... everything is ready!!"
+  exit 0
+fi
